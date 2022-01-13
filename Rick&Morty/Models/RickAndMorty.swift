abc7e2526ef95367bca3e9a51d69458a57931966
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-13 07:59:58 +0300

    Add RickAndMorty model + project navigation struct

---
+++ Rick&Morty/Models/RickAndMorty.swift
@@ -0,0 +1,70 @@
+//
+//  RickAndMorty.swift
+//  Rick&Morty
+//
+//  Created by Станислав on 13.01.2022.
+//
+
+struct RickAndMorty {
+    let info: Info
+    let results: [Character]
+}
+
+struct Info {
+    let pages: Int
+    let next: String?
+    let prev: String?
+}
+
+struct Character {
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let origin: Location
+    let location: Location
+    let image: String
+    let episode: [String]
+    let url: String
+    
+    var description: String {
+        """
+        Name: \(name)
+        Status: \(status)
+        Species: \(species)
+        Gender: \(gender)
+        Origin: \(origin.name)
+        Location: \(location.name)
+        """
+    }
+}
+
+struct Location {
+    let name: String
+}
+
+struct Episode: Decodable {
+    let name: String
+    let date: String
+    let episode: String
+    let characters: [String]
+    
+    var description: String {
+        """
+        Title: \(name)
+        Date: \(date)
+        """
+    }
+    
+    enum CodinKeys: String, CodingKey {
+        case name
+        case date = "air_date"
+        case episode
+        case characters
+    }
+}
+
+enum Link: String {
+    case link = "https://rickandmortyapi.com/api/character"
+}
