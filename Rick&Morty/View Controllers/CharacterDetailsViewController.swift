e3e7474677fe1cb8dd5321c5abb0a35cb3bc9dcc
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-15 13:44:14 +0300

    Add CharacterDetailsVC

---
+++ Rick&Morty/View Controllers/CharacterDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CharacterDetailsViewController.swift
+//  Rick&Morty
+//
+//  Created by Станислав on 15.01.2022.
+//
+
+import UIKit
+
+class CharacterDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
