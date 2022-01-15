09a53f4abb4c4ee108ebca02b2018ba39770922b
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-15 13:50:47 +0300

    Add UI element with IBOutlets and Action

--- Rick&Morty/View Controllers/CharacterDetailsViewController.swift
+++ Rick&Morty/View Controllers/CharacterDetailsViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class CharacterDetailsViewController: UIViewController {
 
+    @IBOutlet var characterImage: UIImageView!
+    @IBOutlet var characterDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    @IBAction func showEpisodesButton(_ sender: UIBarButtonItem) {
     }
 }
