9aff762a699bb3eabf75d165b50150bed23b7df4
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-16 13:37:24 +0300

    Change UIImageView to CharacterImageView + get cached image

--- Rick&Morty/Views/TableViewCell.swift
+++ Rick&Morty/Views/TableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 class TableViewCell: UITableViewCell {
 
     @IBOutlet var characterFullName: UILabel!
-    @IBOutlet var characterImage: UIImageView! {
+    @IBOutlet var characterImage: CharacterImageView! {
         didSet {
             characterImage.layer.cornerRadius = characterImage.frame.height / 2
             characterImage.backgroundColor = .white
@@ -20,11 +20,6 @@ class TableViewCell: UITableViewCell {
     func configure(with character: Character?) {
         characterFullName.text = character?.name
         
-        DispatchQueue.global().async {
-            guard let imageData = ImageManager.shared.fetchImage(from: character?.image) else { return }
-            DispatchQueue.main.async {
-                self.characterImage.image = UIImage(data: imageData)
-            }
-        }
+        characterImage.fetchImage(from: character?.image ?? "")
     }
 }
